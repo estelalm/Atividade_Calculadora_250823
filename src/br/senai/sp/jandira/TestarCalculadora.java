df050cfab000519852df78f6291e0e290a8a09f5
ESTELA ALVES DE MORAES <23111781@CFP127ALUNO.LOCAL>
2023-08-25 11:19:10 -0300

    simplificado

--- src/br/senai/sp/jandira/TestarCalculadora.java
+++ src/br/senai/sp/jandira/TestarCalculadora.java
@@ -16,11 +16,8 @@ public class TestarCalculadora {
 
 		Scanner input = new Scanner(System.in);
 
-		double valor1c1, valor2c1, resultadoc1;
-		double valor1c2, valor2c2, resultadoc2;
-		double valor1c3, valor2c3, resultadoc3;
-		double valor1c4, valor2c4, resultadoc4;
-		char sinalc1, sinalc2, sinalc3, sinalc4;
+		double valor1, valor2, resultado;
+		char sinal;
 
 		System.out.println("C A L C U L A D O R A");
 		System.out.println();
@@ -37,79 +34,79 @@ public class TestarCalculadora {
 
 			System.out.println("--- Cálculo 1");
 			System.out.println("Valor 1: ");
-			valor1c1 = input.nextDouble();
+			valor1 = input.nextDouble();
 			System.out.println("Valor 2: ");
-			valor2c1 = input.nextDouble();
+			valor2 = input.nextDouble();
 			System.out.println("Operação: ");
-			sinalc1 = input.next().charAt(0);
+			sinal = input.next().charAt(0);
 
-			if (sinalc1 == '/' && valor2c1 == 0) {
+			if (sinal == '/' && valor2 == 0) {
 				System.out.println("Não é possível dividir por 0");
-			} else if (sinalc1 != '+' && sinalc1 != '-' && sinalc1 != '*' && sinalc1 != '/') {
+			} else if (sinal != '+' && sinal != '-' && sinal != '*' && sinal != '/') {
 				System.out.println("O sinal não corresponde à nenhuma operação.");
 			} else {
-				resultadoc1 = c1.exibirResultado(valor1c1, valor2c1, sinalc1);
+				resultado = c1.exibirResultado(valor1, valor2, sinal);
 				System.out.println(
-						"Resultado 1 = " + valor1c1 + " " + sinalc1 + " " + valor2c1 + " = " + resultadoc1);
+						"Resultado 1 = " + valor1 + " " + sinal + " " + valor2 + " = " + resultado);
 				System.out.println();
 
 				System.out.println("--- Cálculo 2");
 				System.out.println("Valor 1: ");
-				valor1c2 = input.nextDouble();
+				valor1 = input.nextDouble();
 				System.out.println("Valor 2: ");
-				valor2c2 = input.nextDouble();
+				valor2 = input.nextDouble();
 				System.out.println("Operação: ");
-				sinalc2 = input.next().charAt(0);
+				sinal = input.next().charAt(0);
 				System.out.println("");
 
-				if (sinalc2 == '/' && valor2c2 == 0) {
+				if (sinal == '/' && valor2 == 0) {
 					System.out.println("Não é possível dividir por 0");
-				} else if (sinalc2 != '+' && sinalc2 != '-' && sinalc2 != '*' && sinalc2 != '/') {
+				} else if (sinal != '+' && sinal != '-' && sinal != '*' && sinal != '/') {
 					System.out.println("O sinal não corresponde à nenhuma operação.");
 				} else {
 
-					resultadoc2 = c2.exibirResultado(valor1c2, valor2c2, sinalc2);
-					System.out.println("Resultado 2 = " + valor1c2 + " " + sinalc2 + " " + valor2c2 + " = "
-							+ resultadoc2);
+					resultado = c2.exibirResultado(valor1, valor2, sinal);
+					System.out.println("Resultado 2 = " + valor1 + " " + sinal + " " + valor2 + " = "
+							+ resultado);
 					System.out.println();
 
 					System.out.println("--- Cálculo 3");
 					System.out.println("Valor 1: ");
-					valor1c3 = input.nextDouble();
+					valor1 = input.nextDouble();
 					System.out.println("Valor 2: ");
-					valor2c3 = input.nextDouble();
+					valor2 = input.nextDouble();
 					System.out.println("Operação: ");
-					sinalc3 = input.next().charAt(0);
+					sinal = input.next().charAt(0);
 					System.out.println("");
 
-					if (sinalc3 == '/' && valor2c3 == 0) {
+					if (sinal == '/' && valor2 == 0) {
 						System.out.println("Não é possível dividir por 0");
-					} else if (sinalc3 != '+' && sinalc3 != '-' && sinalc3 != '*' && sinalc3 != '/') {
+					} else if (sinal != '+' && sinal != '-' && sinal != '*' && sinal != '/') {
 						System.out.println("O sinal não corresponde à nenhuma operação.");
 					} else {
 
-						resultadoc3 = c3.exibirResultado(valor1c3, valor2c3, sinalc3);
-						System.out.println("Resultado 3 = " + valor1c3 + " " + sinalc3 + " " + valor2c3
-								+ " = " + resultadoc3);
+						resultado = c3.exibirResultado(valor1, valor2, sinal);
+						System.out.println("Resultado 3 = " + valor1 + " " + sinal + " " + valor2
+								+ " = " + resultado);
 						System.out.println();
 
 						System.out.println("--- Cálculo 4");
 						System.out.println("Valor 1: ");
-						valor1c4 = input.nextDouble();
+						valor1 = input.nextDouble();
 						System.out.println("Valor 2: ");
-						valor2c4 = input.nextDouble();
+						valor2 = input.nextDouble();
 						System.out.println("Operação: ");
-						sinalc4 = input.next().charAt(0);
+						sinal = input.next().charAt(0);
 						System.out.println("");
 
-						if (sinalc4 == '/' && valor2c4 == 0) {
+						if (sinal == '/' && valor2 == 0) {
 							System.out.println("Não é possível dividir por 0");
-						} else if (sinalc4 != '+' && sinalc4 != '-' && sinalc4 != '*' && sinalc4 != '/') {
+						} else if (sinal != '+' && sinal != '-' && sinal != '*' && sinal != '/') {
 							System.out.println("O sinal não corresponde à nenhuma operação.");
 						} else {
-							resultadoc4 = c4.exibirResultado(valor1c4, valor2c4, sinalc4);
-							System.out.println("Resultado 4 = " + valor1c4 + " " + sinalc4 + " " + valor2c4
-									+ " = " + resultadoc4);
+							resultado = c4.exibirResultado(valor1, valor2, sinal);
+							System.out.println("Resultado 4 = " + valor1 + " " + sinal + " " + valor2
+									+ " = " + resultado);
 							System.out.println();
 						}
 
